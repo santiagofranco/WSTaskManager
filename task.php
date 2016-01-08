59a6cbfcf8068ee225576b2f39b2e81a7bf403eb
Santiago <santifg92@gmail.com>
2016-01-08 18:05:07 +0100

    Finished first operation on task.php

--- task.php
+++ task.php
@@ -1,1 +1,45 @@
 <?php
+
+require_once 'daos/DAOTasks.php';
+require_once 'parser/Parser.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
+$parser = new Parser();
+$dao = new DAOTasks();
+
+if( $method === 'GET'){
+	
+	if(isset($_REQUEST['op'])){
+		
+		$op = $_REQUEST['op'];
+		
+		if($op === 'all_tasks'){
+		
+			if(isset($_REQUEST['id'])){
+			
+				$id = $_REQUEST['id'];
+				
+				$tasks = $dao->getTaskByUsuarioId($id);
+				$parser->parsearArray($tasks,'tareas','tarea');
+				
+			}else{
+				$parser->xmlError("Falta algun parametro", 'error');
+			}
+			
+			
+		}else if ($op === 'search_task'){
+			
+		}
+		
+	}else{
+		$parser->xmlError("No se ha indicado la operacion", "error");		
+	}
+	
+	
+}else if ( $method === 'POST'){
+	
+}else if ( $method === 'PUT'){
+	
+}else if ( $method === 'DELETE'){
+	
+}
