eaa0204a85aae206bf660bdb93a65ce2de9afbd9
Santiago <santifg92@gmail.com>
2016-01-14 17:11:43 +0100

    Adding format json to task.php

--- task.php
+++ task.php
@@ -1,46 +1,40 @@
 <?php
-
 require_once 'daos/DAOTasks.php';
 require_once 'parser/Parser.php';
 
-$method = $_SERVER['REQUEST_METHOD'];
-$parser = new Parser();
-$dao = new DAOTasks();
+$method = $_SERVER ['REQUEST_METHOD'];
+$parser = new Parser ();
+$dao = new DAOTasks ();
 
-if( $method === 'GET'){
+if ($method === 'GET') {
 	
-	if(isset($_REQUEST['op'])){
+	if (isset ( $_REQUEST ['op'] )) {
 		
-		$op = $_REQUEST['op'];
+		$op = $_REQUEST ['op'];
 		
-		if($op === 'all_tasks'){
-		
-			if(isset($_REQUEST['id'])){
+		if ($op === 'all_tasks') {
 			
-				$id = $_REQUEST['id'];
+			if (isset ( $_REQUEST ['id'] )) {
 				
-				$tasks = $dao->getTaskByUsuarioId($id);
-				if(isset($_REQUEST['formato']) && $_REQUEST['formato'] == 'json')
-					echo json_encode($tasks);
+				$id = $_REQUEST ['id'];
+				
+				$tasks = $dao->getTaskByUsuarioId ( $id );
+				if (isset ( $_REQUEST ['formato'] ) && $_REQUEST ['formato'] == 'json')
+					echo json_encode ( $tasks );
 				else
-					$parser->parsearArray($tasks,'tareas','tarea');
-				
-			}else{
-				$parser->xmlError("Falta algun parametro", 'error');
+					$parser->parsearArray ( $tasks, 'tareas', 'tarea' );
+			} else {
+				if (isset ( $_REQUEST ['formato'] ) && $_REQUEST ['formato'] == 'json')
+					echo '{"error":"Falta algun parametro"}';
+					
+				else
+					$parser->xmlError ( "Falta algun parametro", 'error' );
 			}
-			
-			
 		}
-		
-	}else{
-		$parser->xmlError("No se ha indicado la operacion", "error");		
+	} else {
+		$parser->xmlError ( "No se ha indicado la operacion", "error" );
 	}
-	
-	
-}else if ( $method === 'POST'){
-	
-}else if ( $method === 'PUT'){
-	
-}else if ( $method === 'DELETE'){
-	
+} else if ($method === 'POST') {
+} else if ($method === 'PUT') {
+} else if ($method === 'DELETE') {
 }
--- test.php
+++ test.php
@@ -1,30 +1,12 @@
 <?php
 require 'daos/DAOTasks.php';
-require 'Config.php';
 require 'db/DBConnect.php';
 require 'parser/Parser.php';
 
 $dao = new DAOTasks ();
 $parser = new Parser();
 
-if(isset($_REQUEST['op'])){
-	
-	$op = $_REQUEST['op'];
-
-	if($op === 'user'){
-		if(!isset($_REQUEST['id'])) $parser->xmlError("Falta el parametro id");
-		else{
-			$tasks = $dao->getTaskByUsuarioId($_REQUEST['id']);
-			$parser->parsearArray($tasks,"tasks","task");
-		}
-	}else if ($op === 'search'){
-		if(!isset($_REQUEST['string'])) $parser->xmlError("Falta el parametro string");
-		else{
-			$tasks = $dao->getTaskBySearchString($_REQUEST['string']);
-			$parser->parsearArray($tasks,"tasks","task");
-		}
-	}
-	
-}else $parser->parsearArray($dao->getTasks(),"tasks","task");
+$tasks = $dao->getTasks();
+echo json_encode($tasks);
 
 
